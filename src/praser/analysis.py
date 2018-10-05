2c17c2f6f701cfa665a4359792d10d21669f4c47
jackwen <894362844@qq.com>
2018-10-05 19:17:27 +0800

    parser

--- src/praser/analysis.py
+++ src/praser/analysis.py
@@ -10,6 +10,7 @@ end = ('SEMI','ID','STRUCT','RETURN','IF','ELSE','WHILE',
 'RC','LC','RP','LP','FLOAT','INT','NOT','DOT','OR','AND',
 'DIV','STAR','MINUS','PLUS','RELOP','ASSIGNOP','COMMA')
 
+
 p = 0
 value = ''
 token = ''
@@ -19,72 +20,94 @@ content = ''
 line = 0
 state = ''
 
+list = []
+
 list_value = []
 list_syn = []
 list_line = []
 m = 0
 
 def Program():
-    ExtDefList()
+    global L
+    if ExtDefList():
+        print("success")
 
 
 def ExtDefList():
-    global list_line, list_syn, list_value, m
-    m_tmp = m
-    num = 0
+    global list_line, list_syn, list_value
+    n = 0
+    list.append('ExtDefList')
     while ExtDef():
-        num += 1
-    if num >= 1:
-        return True
-    else:
-        return False
+        n += 1
+    return True
 
 def ExtDef():
     global list_line, list_syn, list_value, m
     m_tmp = m
-    if Specifier():
+    n = 0
+    if list_syn[m] == '#':
+        return False
+    elif Specifier():
         if FunDec():
             if CompSt():
                 return True
+            while list.pop() == 'ExtDef':
+                n += 1
             return False
         elif ExtDecList():
             if SEMI():
                 return True
             else :
+                while list.pop() == 'ExtDef':
+                    n += 1
                 return False
         else:
             if SEMI():
                 return True
     else:
+        while list.pop() == 'ExtDef':
+            n += 1
         return False
 
 def ExtDecList():
-    global list_line, list_syn, list_value, m
+    global list_line, list_syn, list_value, m,L
+    n = 0
+    list.append('ExtDecList')
     m_tmp = m
     if VarDec():
         if COMMA():
             if ExtDecList():
                 return True
+            while list.pop() == 'ExtDecList':
+                n += 1
             m = m_tmp
             return False
         return True
+    while list.pop() == 'ExtDecList':
+        n += 1
     m = m_tmp
     return False
 
 
 def Specifier():
     global list_line, list_syn, list_value, m
+    list.append('Specifier')
+    n = 0
     m_tmp = m
     if TYPE():
         return True
     elif StructSpecifier():
         return True
     else:
+        while list.pop() == 'Specifier':
+            n += 1
         m = m_tmp
         return False
 
 def StructSpecifier():
     global list_line, list_syn, list_value, m
+    list.append('StructSpecifier')
+    n = 0
     m_tmp = m
     if STRUCT():
         if LC():
@@ -92,27 +115,42 @@ def StructSpecifier():
                 if RC():
                     return True
                 else:
+                    while list.pop() == 'StructSpecifier':
+                        n += 1
                     m = m_tmp
                     return False
             else:
+                while list.pop() == 'StructSpecifier':
+                    n += 1
                 m = m_tmp
                 return False
         else:
+            while list.pop() == 'StructSpecifier':
+                n += 1
             m = m_tmp
             return False
+    while list.pop() == 'StructSpecifier':
+        n += 1
     m = m_tmp
     return False
 
 def VarDec():
+    list.append('VarDec')
+    n = 0
     global list_line, list_syn, list_value, m
     m_tmp = m
     if ID():
         return True
     else:
+        while list.pop() == 'VarDec':
+            n += 1
+        m = m_tmp
         return False
 
 def FunDec():
     global list_line, list_syn, list_value, m
+    list.append('FunDec')
+    n = 0
     m_tmp = m
     if ID():
         if LP():
@@ -122,13 +160,20 @@ def FunDec():
             else:
                 if RP():
                     return True
+        while list.pop() == 'FunDec':
+            n += 1
         m = m_tmp
+        return False
+    while list.pop() == 'FunDec':
+        n += 1
     m = m_tmp
     return False
 
 
 def VarList():
     global list_line, list_syn, list_value, m
+    list.append('VarList')
+    n = 0
     m_tmp = m
     if ParamDec():
         if COMMA():
@@ -136,18 +181,28 @@ def VarList():
                 return True
         else:
             return True
+    while list.pop() == 'VarList':
+        n += 1
+    m = m_tmp
     return False
 
 
 def ParamDec():
     global list_line, list_syn, list_value, m
+    list.append('ParamDec')
+    n = 0
     m_tmp = m
     if Specifier():
         if VarDec():
             return True
+    while list.pop() == 'ParamDec':
+        n += 1
+    m = m_tmp
     return False
 
 def CompSt():
+    list.append('CompSt')
+    n = 0
     global list_line, list_syn, list_value, m
     m_tmp = m
     if LC():
@@ -156,19 +211,28 @@ def CompSt():
                 if RC():
                     return True
                 else:
+                    while list.pop() == 'CompSt':
+                        n += 1
                     m = m_tmp
             else:
+                while list.pop() == 'CompSt':
+                    n += 1
                 m = m_tmp
                 return False
         else:
+            while list.pop() == 'CompSt':
+                n += 1
             m = m_tmp
             return False
     else:
+        while list.pop() == 'CompSt':
+            n += 1
         m = m_tmp
         return False
 
 def StmtList():
     global list_line, list_syn, list_value, m
+    list.append('StmtList')
     m_tmp = m
     num = 0
     while Stmt():
@@ -178,15 +242,23 @@ def StmtList():
 
 def Stmt():
     global list_line, list_syn, list_value, m
+    list.append('Stmt')
+    n = 0
     m_tmp = m
     if Exp():
         if SEMI():
             return True
         else:
+            while list.pop() == 'Stmt':
+                n += 1
             m = m_tmp
             return False
     elif CompSt():
         return True
+    elif RETURN():
+        if Exp():
+            if SEMI():
+                return True
     elif IF():
         if LP():
             if Exp():
@@ -196,19 +268,30 @@ def Stmt():
                             if Stmt():
                                 return True
                             else:
+                                while list.pop() == 'Stmt':
+                                    n += 1
                                 m = m_tmp
                                 return False
                         else:
-                            m = m_tmp
-                            return False
-                    return True
+                            return True
+                    else:
+                        while list.pop() == 'Stmt':
+                            n += 1
+                        m = m_tmp
+                        return False
                 else:
+                    while list.pop() == 'Stmt':
+                        n += 1
                     m = m_tmp
                     return False
             else:
+                while list.pop() == 'Stmt':
+                    n += 1
                 m = m_tmp
                 return False
         else:
+            while list.pop() == 'Stmt':
+                n += 1
             m = m_tmp
             return False
     elif WHILE():
@@ -218,23 +301,34 @@ def Stmt():
                     if Stmt():
                         return True
                     else:
+                        while list.pop() == 'Stmt':
+                            n += 1
                         m = m_tmp
                         return False
                 else:
+                    while list.pop() == 'Stmt':
+                        n += 1
                     m = m_tmp
                     return False
             else:
+                while list.pop() == 'Stmt':
+                    n += 1
                 m = m_tmp
                 return False
         else:
+            while list.pop() == 'Stmt':
+                n += 1
             m = m_tmp
             return False
     else:
+        while list.pop() == 'Stmt':
+            n += 1
         m = m_tmp
         return False
 
 def DefList():
     global list_line, list_syn, list_value, m
+    list.append('DefList')
     m_tmp = m
     num = 0
     while Def():
@@ -243,43 +337,71 @@ def DefList():
 
 def Def():
     global list_line, list_syn, list_value, m
+    list.append('Def')
+    n = 0
     m_tmp = m
     if Specifier():
         if DecList():
             if SEMI():
                 return True
             else:
+                while list.pop() == 'Def':
+                    n += 1
                 m = m_tmp
                 return False
         else:
+            while list.pop() == 'Def':
+                n += 1
             m = m_tmp
             return False
     else:
+        while list.pop() == 'Def':
+            n += 1
+        m = m_tmp
         return False
 
 def DecList():
     global list_line, list_syn, list_value, m
+    list.append('DecList')
+    n = 0
     m_tmp = m
     if Dec():
         if COMMA():
             if DecList():
                 return True
+            while list.pop() == 'DecList':
+                n += 1
+            m = m_tmp
+            return  False
         return True
+    while list.pop() == 'DecList':
+        n += 1
+    m = m_tmp
     return False
 
 def Dec():
     global list_line, list_syn, list_value, m
+    list.append('Dec')
+    n = 0
     m_tmp = m
     if VarDec():
         if ASSIGNOP():
             if Exp():
                 return True
+            while list.pop() == 'Dec':
+                n += 1
+            m = m_tmp
             return False
         return True
+    while list.pop() == 'Dec':
+        n += 1
+    m = m_tmp
     return False
 
 def Exp():
     global list_line, list_syn, list_value, m
+    list.append('Exp')
+    n = 0
     m_tmp = m
     if LP():
         if Exp():
@@ -287,24 +409,38 @@ def Exp():
                 if Exp1():
                     return True
                 else:
+                    while list.pop() == 'Exp':
+                        n += 1
                     m = m_tmp
                     return False
             else:
+                while list.pop() == 'Exp':
+                    n += 1
                 m = m_tmp
         else:
+            while list.pop() == 'Exp':
+                n += 1
             m = m_tmp
     elif MINUS():
         if Exp():
             if Exp1():
                 return True
             else:
+                while list.pop() == 'Exp':
+                    n += 1
                 m = m_tmp
         else:
+            while list.pop() == 'Exp':
+                n += 1
             m = m_tmp
     elif NOT():
         if Exp():
             if Exp1():
                 return True
+        else:
+            while list.pop() == 'Exp':
+                n += 1
+            m = m_tmp
     elif ID():
         if LP():
             if Args():
@@ -319,12 +455,25 @@ def Exp():
     elif INT():
         if Exp1():
             return True
+        else:
+            while list.pop() == 'Exp':
+                n += 1
+            m = m_tmp
     elif FLOAT():
         if Exp1():
             return True
+        else:
+            while list.pop() == 'Exp':
+                n += 1
+            m = m_tmp
+    else:
+        while list.pop() == 'Exp':
+            n += 1
+        m = m_tmp
 
 def Exp1():
     global list_line, list_syn, list_value, m
+    list.append('Exp1')
     m_tmp = m
     if ASSIGNOP():
         if Exp():
@@ -355,23 +504,39 @@ def Exp1():
 
 def Args():
     global list_line, list_syn, list_value, m
+    list.append('Args')
+    n = 0
     m_tmp = m
     if Exp():
         if COMMA():
             if Args():
                 return True
+            while list.pop() == 'Args':
+                n += 1
+            m = m_tmp
             return False
         return True
+    while list.pop() == 'Args':
+        n += 1
+    m = m_tmp
     return False
 
 def TYPE():
     global list_line, list_syn, list_value, m
+    list.append('TYPE')
+    n = 0
     m_tmp = m
-    if INT():
+    if list_syn[m] == 'INT':
+        m += 1
+        list.append(':INT')
         return True
-    elif FLOAT():
+    elif list_syn[m] == 'FLOAT':
+        list.append(':FLOAT')
+        m += 1
         return True
     else:
+        while list.pop() == 'TYPE':
+            n += 1
         m = m_tmp
         return False
 
@@ -380,7 +545,7 @@ def COMMA():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'COMMA':
-        print('COMMA')
+        list.append('COMMA')
         m += 1
         return True
 
@@ -389,7 +554,7 @@ def ASSIGNOP():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'ASSIGNOP':
-        print('ASSIGNOP')
+        list.append('ASSIGNOP')
         m += 1
         return True
 
@@ -398,7 +563,7 @@ def RELOP():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'RELOP':
-        print('RELOP')
+        list.append('RELOP')
         m += 1
         return True
 
@@ -407,7 +572,7 @@ def PLUS():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'PLUS':
-        print('PLUS')
+        list.append('PLUS')
         m += 1
         return True
 
@@ -416,7 +581,7 @@ def MINUS():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'MINUS':
-        print('MINUS')
+        list.append('MINUS')
         m += 1
         return True
 
@@ -425,7 +590,7 @@ def STAR():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'STAR':
-        print('STAR')
+        list.append('STAR')
         m += 1
         return True
 
@@ -435,7 +600,7 @@ def DIV():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'DIV':
-        print('DIV')
+        list.append('DIV')
         m += 1
         return True
 
@@ -444,7 +609,7 @@ def AND():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'AND':
-        print('AND')
+        list.append('AND')
         m += 1
         return True
 
@@ -453,7 +618,7 @@ def OR():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'OR':
-        print('OR')
+        list.append('OR')
         m += 1
         return True
 
@@ -462,7 +627,7 @@ def DOT():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'DOT':
-        print('DOT')
+        list.append('DOT')
         m += 1
         return True
 
@@ -470,7 +635,7 @@ def NOT():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'NOT':
-        print('NOT')
+        list.append('NOT')
         m += 1
         return True
 
@@ -478,7 +643,7 @@ def INT():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'INT':
-        print('INT')
+        list.append('INT')
         m += 1
         return True
 
@@ -486,7 +651,7 @@ def FLOAT():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'FLOAT':
-        print('FLOAT')
+        list.append('FLOAT')
         m += 1
         return True
 
@@ -495,7 +660,7 @@ def LP():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'LP':
-        print('LP')
+        list.append('LP')
         m += 1
         return True
 
@@ -503,7 +668,7 @@ def RP():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'RP':
-        print('RP')
+        list.append('RP')
         m += 1
         return True
 
@@ -511,7 +676,7 @@ def LC():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'LC':
-        print('LC')
+        list.append('LC')
         m += 1
         return True
 
@@ -519,7 +684,7 @@ def RC():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'RC':
-        print('RC')
+        list.append('RC')
         m += 1
         return True
 
@@ -527,7 +692,7 @@ def STRUCT():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'STRUCT':
-        print('STRUCT')
+        list.append('STRUCT')
         m += 1
         return True
 
@@ -535,7 +700,7 @@ def RETURN():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'RETURN':
-        print('RETURN')
+        list.append('RETURN')
         m += 1
         return True
 
@@ -543,7 +708,7 @@ def IF():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'IF':
-        print('IF')
+        list.append('IF')
         m += 1
         return True
 
@@ -551,7 +716,7 @@ def ELSE():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'ELSE':
-        print('ELSE')
+        list.append('ELSE')
         m += 1
         return True
 
@@ -559,7 +724,7 @@ def WHILE():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'WHILE':
-        print('WHILE')
+        list.append('WHILE')
         m += 1
         return True
 
@@ -568,7 +733,7 @@ def ID():
     global list_line, list_syn, list_value, m
     m_tmp = m
     if list_syn[m] == 'ID':
-        print('ID')
+        list.append('ID')
         m += 1
         return True
 
@@ -576,9 +741,9 @@ def ID():
 def SEMI():
     global list_line, list_syn, list_value, m
     if list_syn[m] == 'SEMI':
-        print('SEMI')
+        list.append('SEMI')
         m += 1
-        return True  # SEMI匹配后跳
+        return True
 
 
 if __name__ == '__main__':
@@ -586,7 +751,21 @@ if __name__ == '__main__':
     content = lexer.clear_comment(content)
     while syn != "#":
         p, syn, value, line = lexer.get_token(content, p, line)
+        if syn == 'error2':
+            print('string ' + value + ' 不封闭! Error in line ' + str(line))
+        elif syn == 'error3':
+            print('number ' + value + ' 错误，不能以0开头! Error in line ' + str(line))
+        elif syn == 'error4':
+            print('char ' + value + ' 不封闭! Error in line ' + str(line))
+        elif syn == 'error5':
+            print('number ' + value + ' 不合法! Error in line ' + str(line))
+        elif syn == 'error6':
+            print('identifier' + value + ' 不能包含非法字符!Error in line ' + str(line))
+        elif syn == 'error7':
+            print('number ' + value + ' 不合法,包含字母! Error in line ' + str(line))
         list_line.append(line)
         list_syn.append(syn)
         list_value.append(value)
     Program()
+    for item in list:
+        print(item)
